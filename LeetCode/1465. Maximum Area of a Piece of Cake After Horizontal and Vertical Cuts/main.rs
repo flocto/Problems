84e0c1dbc129a59e0caa6a7cac275d21cb9f7ca1
flocto <alexzhang05@gmail.com>
2022-07-01 20:53:18 -0500

    1465. Maximum Area of a Piece of Cake (Solved)

--- LeetCode/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts/main.rs
+++ LeetCode/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts/main.rs
@@ -1,4 +1,11 @@
 impl Solution {
+    pub fn get_max_dist(vec: Vec<i64>) -> i64 {
+        let mut max = 0;
+        for i in 0..vec.len()-1{
+            max = max.max(vec[i+1]-vec[i]);
+        }
+        max
+    }
     pub fn max_area(h: i32, w: i32, horizontal_cuts: Vec<i32>, vertical_cuts: Vec<i32>) -> i32 {
         let mut h_cuts: Vec<i64> = horizontal_cuts.iter().map(|&x| x as i64).collect();
         let mut v_cuts: Vec<i64> = vertical_cuts.iter().map(|&x| x as i64).collect();
@@ -8,16 +15,9 @@ impl Solution {
         v_cuts.push(w as i64);
         h_cuts.sort();
         v_cuts.sort();
-        let max_height = get_max_dist(h_cuts);
-        let max_width = get_max_dist(v_cuts);
+        let max_height = Solution::get_max_dist(h_cuts);
+        let max_width = Solution::get_max_dist(v_cuts);
         let mut max_area = max_height * max_width;
         (max_area % 1_000_000_007) as i32
-    }
-    pub fn get_max_dist(vec: Vec<i64>) -> i64 {
-        let mut max = 0;
-        for i in 0..vec.len()-1{
-            max = max.max(vec[i+1]-vec[i]);
-        }
-        max
     }
 }
